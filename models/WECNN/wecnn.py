f0532c5e588878e43b24e7b0ae9a26cca1904ef6
Gafran <d1amondb0t@outlook.com>
2026-04-24 09:18:37 -0400

    WECNN

--- models/WECNN/wecnn.py
+++ models/WECNN/wecnn.py
@@ -4,14 +4,14 @@ import torch.nn.functional as F
 
 class WECNN(nn.Module):
   
-  def __init__(self, vocab_size, embedding_dim=300, filters=[2,3,4,6,8], num_filters=200):
+  def __init__(self, vocab_size, embedding_dim=300, filters=[2,3,4,6,8], num_filters=200, pad_idx=0):
     super(WECNN, self).__init__()
     
     # Embedding Layer Question Pair & Unique Words
-    self.embedding = nn.Embedding(vocab_size, embedding_dim)
+    self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=pad_idx)
 
     # Keyword Embdding and Freezing Weights - Section 3.5
-    self.keyword_embedding = nn.Embedding(vocab_size, embedding_dim)
+    self.keyword_embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=pad_idx)
     self.keyword_embedding.weight.requires_grad = False
     
     self.filters = filters
@@ -27,69 +27,76 @@ class WECNN(nn.Module):
     # Initialization of conv layers with Xavier Uniform
     for conv in self.convs:
       nn.init.xavier_uniform_(conv.weight)
+      if conv.bias is not None:
+        nn.init.zeros_(conv.bias)
       
     # lambda
-    self.lam = nn.Parameter(torch.ones(1))
+    self.lam_raw = nn.Parameter(torch.tensor(10.0))
     
     # Dropout=0.1 as paper specs  
     self.dropout = nn.Dropout(0.1)
   
     self.bn = nn.BatchNorm1d(embedding_dim, momentum=0.7)
     
-    # Concatenation layer with n_filters * 3 (cos(c_min), cos(c_max), cos(c_max_i))
+    # Concatenation layer with n_filters * 3  * (cos(c_min), cos(c_max), cos(c_max_i))
     # Output is 2 since they are similar or not
-    self.fc = nn.Linear(len(self.filters)*3, 2)
+    self.fc = nn.Linear(len(self.filters) * 3 * 3, 2)
+    
+  def _embed(self, x, is_keyword=False):
+    emb = self.keyword_embedding(x) if is_keyword else self.embedding(x)  # (B, L, D)
+    emb = emb.transpose(1, 2)                                             # (B, D, L)
+    emb = self.bn(emb)
+    return emb
     
   def get_features(self, x, isKeyword=False):
-    emb_layer = self.keyword_embedding if isKeyword else self.embedding
-    x = self.bn(emb_layer(x).transpose(1, 2))
+    x = self._embed(x, is_keyword=isKeyword)
     
     all_pools = []
     for conv in self.convs:
-      c = self.dropout(F.relu(conv(x)))
+      c = self.dropout(F.relu(conv(x))) # (B, num_filters, L')
       
       c_max = torch.max(c, dim=2)[0]
       c_min = torch.min(c, dim=2)[0]
       
-      chunks = torch.chunk(c, 4, dim=1)
-      c_max_i = torch.stack([torch.max(chunk.view(chunk.size(0), -1), dim=1)[0] 
-                             for chunk in chunks])
-      all_pools.append(c_max, c_min, c_max_i)
+      # chunked max over 4 equal chunks along time dimension
+      chunks = torch.chunk(c, 4, dim=2)
+      chunk_maxes = [torch.max(chunk, dim=2)[0] for chunk in chunks] # (B, num_filters)
+      c_max_i = torch.cat(chunk_maxes, dim=1) # (B, 4 * num_filters)
+      
+      all_pools.append((c_max, c_min, c_max_i))
     return all_pools
   
   def cosine_sim(self, f1, f2):
     dot = torch.sum(f1 * f2, dim=1)
     norm = torch.norm(f1, 2, 1) * torch.norm(f2, 2, 1)
     epsilon = 1e-8
+    lam = torch.sigmoid(self.lam_raw)
     # Not compute division by zero
-    return (dot / (norm + epsilon))  + self.lam
+    return (dot / (norm + epsilon))  + lam
   
-  def forward_propagation(self, p, q, p_u, q_u, p_k, q_k):
+  def forward(self, p, q, p_u, q_u, p_k, q_k, probs=False):
     
     # Get all values for each embeddings  
-    feat_p = self.get_features(p)
+    feat_p   = self.get_features(p)
     feat_p_u = self.get_features(p_u)
     feat_p_k = self.get_features(p_k, True)
     
-    feat_q = self.get_features(q)
+    feat_q   = self.get_features(q)
     feat_q_u = self.get_features(q_u)
     feat_q_k = self.get_features(q_k, True)
     
     combined_scores = []
     for i in range(len(self.filters)):
       for p_feat, q_feat in [(feat_p, feat_q), (feat_p_u, feat_q_u), (feat_p_k, feat_q_k)]:
-        combined_scores.append(self.cosine_sim(p_feat[i][0], q_feat[i][0]))
-        combined_scores.append(self.cosine_sim(p_feat[i][1], q_feat[i][1]))
-        combined_scores.append(self.cosine_sim(p_feat[i][2], q_feat[i][2]))
+        combined_scores.append(self.cosine_sim(p_feat[i][0], q_feat[i][0])) # c_max
+        combined_scores.append(self.cosine_sim(p_feat[i][1], q_feat[i][1])) # c_min
+        combined_scores.append(self.cosine_sim(p_feat[i][2], q_feat[i][2])) # c_max_i
 
-    x = torch.stack(combined_scores, dim=1)
+    x = torch.stack(combined_scores, dim=1) # (B, 45)
     
-    output = self.fc(x)
+    logits = self.fc(x)                     # (B, 2)
     
-    return F.softmax(output, dim=1)
-  
-  def train(self):
-    pass
-  
-  def test(self):
-    pass
+    if probs == True:
+      return F.softmax(logits, dim=1)
+    
+    return logits
